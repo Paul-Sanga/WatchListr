0b298d780472dea7c340487d94875a5a89d59685
projectfinalaudio <irfanghat@gmail.com>
2023-09-11 15:22:46 +0300

    Source files

---
+++ BACK_END/watchlistr_api/src/main.rs
@@ -0,0 +1,22 @@
+use axum::{response::IntoResponse, routing::get, Json, Router};
+
+async fn health_checker_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "Rust | Axum API | MySQL";
+    // DEFINE A STRUCT FOR RESPONSE MESSAGE
+    let json_response = serde_json::json!({
+        "status": "success",
+        "message": MESSAGE
+    });
+    Json(json_response)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/healthchecker", get(health_checker_handler));
+
+    println!("Server is listening...");
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
