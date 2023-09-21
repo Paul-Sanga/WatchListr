68125a256a7471c25af5c7fd79bbbcf8fbf5dedc
projectfinalaudio <irfanghat@gmail.com>
2023-09-21 22:44:24 +0300

    Created new image on update failure | Consult docs or expert for guidance for future deployments

--- FRONT_END/src/app/environment/environment.ts
+++ FRONT_END/src/app/environment/environment.ts
@@ -4,7 +4,7 @@ export const env = {
 
     RapidAPIKey: "c2b86ae9b0mshc1db424ded6bb9ap1cfbd4jsnd005efd92930",
 
-    // WatchListrAPIurl: "https://watchlistr-api.onrender.com"
-    WatchListrAPIurl: "http://localhost:3000"
+    WatchListrAPIurl: "https://watchlistr-api.onrender.com"
+    // WatchListrAPIurl: "http://localhost:3000"
 }
 
